ea2af4365e414ece70f102883582ca0842568fb4
paola <paola@DESKTOP-N4G7BIV>
2019-03-25 19:02:58 +0100

    Completati punti 5 e 6.

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -12,32 +12,43 @@ public class Libretto {
 		this.voti = new ArrayList<Voto>(); 
 	}
 
-	public void add(int voto, String corso, LocalDate data) {
+	/*public void add(int voto, String corso, LocalDate data) {
 
-	}
-	
+	}*/
+
 	/**
 	 * Aggiunge un nuovo voto al libretto
 	 * 
 	 * @param v il {@link Voto} da aggiungere
+	 * @return {@code true} nel caso normale, {@code false} se non e'
+	 * 		   riuscito ad aggiungere il voto.
 	 */
-	
-	public void add(Voto v) {
-		voti.add(v);
+
+	public boolean add(Voto v) {
+		if(!this.esisteGiaVoto(v) && !this.votoConflitto(v)) {
+			voti.add(v);
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
+	
+	// Abbiamo potenziato il metodo add in modo da rifiutare
+	// l'inserimento di dati errati
 
 	/*
 	public void StampaVoti(int voto) {
-		
+
 	}
-	
+
 	public String StampaVoti2(int voto) {
-		
+
 	}*/
-	
+
 	// Mi conviene utilizzare la terza versione List<Voto>
 	// perche' avro' un'interfaccia grafica
-	
+
 	/**
 	 * Seleziona il sottoinsieme di voti che hanno il punteggio specificato
 	 * 
@@ -45,9 +56,9 @@ public class Libretto {
 	 * @return lista di [{@link Voto} aventi quel punteggio (eventualmente vuota)
 	 */
 	public List<Voto> cercaVoti(int punti) { 
-		
+
 		List<Voto> result = new ArrayList<Voto>();
-		
+
 		for(Voto v : this.voti) {
 			if(v.getPunti()==punti) {
 				result.add(v);
@@ -55,10 +66,10 @@ public class Libretto {
 				// un oggetto che gia' c'era
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	/**
 	 * Ricerca un {@link Voto} relativo al corso di cui e' specificato il nome
 	 *
@@ -72,26 +83,26 @@ public class Libretto {
 				return v;
 			}
 		}*/
-		
+
 		// Esiste sia l'indexOf che il lastIndexOf a seconda che si voglia cercare
 		// dall'inizio oppure dalla fine
-		
+
 		// indexOf() vuole un oggetto della lista quindi della classe voto
 		// ma io non ho nessuna informazione di questo oggetto se non il nomeEsame
 		// --------> quindi gli fornisco un oggetto costruito con lo stesso nomeEsame
 		// ma valori casuali per gli altri attributi
 		// perche' indexOf richiama il metodo equals che confrontera' soltanto il nomeEsame
 		// quindi gli altri campi non verranno mai consultati
-		
+
 		Voto voto = new Voto(0, nomeEsame, null);
-		
+
 		int posizione = this.voti.indexOf(voto);
 		if(posizione == -1)
 			return null;
 		else
 			return this.voti.get(posizione);
 	}
-	
+
 	/**
 	 * Dato un {@link Voto}, determina se esiste gia' un voto con uguale
 	 * corso ed uguale punteggio.
@@ -102,21 +113,21 @@ public class Libretto {
 	 * 		   con voto diverso
 	 */
 	public boolean esisteGiaVoto(Voto v) {
-		
+
 		int posizione = this.voti.indexOf(v);
 		if(posizione == -1)
 			return false;
 		else
 			return (v.getPunti() == this.voti.get(posizione).getPunti());
-		
+
 		/*else {
 			if(v.getPunti() == this.voti.get(posizione).getPunti())
 				return true;
 			else 
 				return false;
 		}*/
-			
-		
+
+
 		/*Voto trovato = this.cercaEsame(v.getCorso());
 		if(trovato==null) {
 			return false;
@@ -127,5 +138,27 @@ public class Libretto {
 		else {
 			return false;
 		}*/
+	}
+
+	/**
+	 * Mi dice se il {@link Voto} {@code v} e' in conflitto con uno dei voti esistenti.
+	 * Se il voto non esiste, non c'e' conflitto. Se esiste ed ha punteggio diverso,
+	 * c'e' conflitto.
+	 * 
+	 * @param v
+	 * @return {@code true} se il voto esiste ed ha un punteggio diverso,
+	 * 		   {@code false} se il voto non esiste, oppure esiste ma ha lo stesso
+	 * 		   punteggio.
+	 */
+	public boolean votoConflitto(Voto v) {
+		int posizione = this.voti.indexOf(v);
+		if(posizione == -1)
+			return false;
+		else
+			return (v.getPunti() != this.voti.get(posizione).getPunti());
+	}
+
+	public String toString() {
+		return this.voti.toString();
 	}
 }
--- src/it/polito/tdp/libretto/TestLibretto.java
+++ src/it/polito/tdp/libretto/TestLibretto.java
@@ -39,6 +39,16 @@ public class TestLibretto {
 		System.out.format("Il voto %s è %s\n", giusto.toString(), libr.esisteGiaVoto(giusto));
 		System.out.format("Il voto %s è %s\n", sbagliato.toString(), libr.esisteGiaVoto(sbagliato));
 		System.out.format("Il voto %s è %s\n", mancante.toString(), libr.esisteGiaVoto(mancante));
+		
+		System.out.format("Il voto %s è in conflitto %s\n", giusto.toString(), libr.votoConflitto(giusto));
+		System.out.format("Il voto %s è in conflitto %s\n", sbagliato.toString(), libr.votoConflitto(sbagliato));
+		System.out.format("Il voto %s è in conflitto %s\n", mancante.toString(), libr.votoConflitto(mancante));
+		
+		System.out.println(libr.add(giusto));
+		System.out.println(libr.add(sbagliato));
+		System.out.println(libr.add(mancante));
+		
+		System.out.println(libr.toString());
 	}
 
 }
